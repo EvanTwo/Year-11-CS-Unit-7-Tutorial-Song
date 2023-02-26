3d01407074521f196c931c4500c5d388fc4818b6
EvanTwo <cht773@student.cis.edu.hk>
2023-02-26 10:52:11 +0800

    commit

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    ArrayList<String> newList = new ArrayList<>();
+    private int counter;
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> listeners){
+        counter = 0;
+        for(int i =0;i< listeners.size();i++){
+            listeners.add(listeners.get(0).toLowerCase());
+            listeners.remove(0);
+        }
+
+
+        for(int i = 0; i<listeners.size();i++){
+            if(!newList.contains(listeners.get(i))){
+                newList.add(listeners.get(i));
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
